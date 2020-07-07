c2b1b2c52387f5a1ebc2c98da32d5bcd20f04913
jiaoshengzhi <11194720@qq.com>
2020-07-07 11:02:30 +0800

    文档补全

--- src/views/changelog.vue
+++ src/views/changelog.vue
@@ -1,26 +1,55 @@
 <template>
-  <div class="AppManage">
+  <div class="changelog">
     <div class="titlePart cf">
-      <strong class="h1">应用管理</strong>
+      <strong class="h1">更新日志</strong>
     </div>
-    <!-- ep-input 必须要包裹在 el-form 内 -->
-    <el-form>
-      <ep-input label="input"></ep-input>
-    </el-form>
+    <div class="contPart">
+      <div class="secition">
+        <h3>1.0.0</h3>
+        <dl>
+          <dt>补充说明模块</dt>
+          <dd>添加更新日志、使用说明模块。</dd>
+          <dt>初始化表单组件</dt>
+          <dd>添加input、select、button组件并进行优化改造</dd>
+          <dt>初始化系统</dt>
+          <dd>初始化系统，并对系统进行改造。</dd>
+        </dl>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="less" scope>
-.AppManage{}
-.titlePart {
-}
-.contPart{
-  margin-bottom: 10px;
-  .creatApp{
-    display: inline-block;
-    margin: 20px 0 5px;
-    color: #02A7F0;
-    font-size: 18px;
+.changelog{
+  .secition{
+    padding: 20px;
+    margin-bottom: 50px;
+    h3{
+      // line-height: 1.4;
+      border-bottom: 1px solid #d2d2d2;
+      margin-bottom: 15px;
+    }
+    dl{
+      padding-left: 20px;
+    }
+    dt{
+      font-size: 16px;
+    }
+    dd{
+      position: relative;
+      padding-left: 20px;
+      margin-bottom: 20px;
+      color: #666;
+    }
+    dd::before{
+      position: absolute;
+      content: '·';
+      display: block;
+      left: 7px;
+      color: #666;
+      font-size: 28px;
+      line-height: 24px;
+      }
   }
 }
 </style>
@@ -29,7 +58,7 @@
 <script>
 // @ is an alias to /src
 export default {
-  name: 'AppManage',
+  name: 'changelog',
   data() {
     return {
       input:''
--- src/views/guide.vue
+++ src/views/guide.vue
@@ -1,26 +1,40 @@
 <template>
-  <div class="AppManage">
+  <div class="guide">
     <div class="titlePart cf">
-      <strong class="h1">应用管理</strong>
+      <strong class="h1">开发指南</strong>
     </div>
-    <!-- ep-input 必须要包裹在 el-form 内 -->
-    <el-form>
-      <ep-input label="input"></ep-input>
-    </el-form>
+    <div class="contPart">
+
+      <div class="secition">
+        <p>推荐使用 npm 的方式安装，它能更好地和 webpack 打包工具配合使用。</p>
+        <pre v-highlightjs><code class="code">npm i epd-ui -S</code></pre>
+      </div>
+
+      <div class="secition">
+        <h3>完整引入</h3>
+        <pre v-highlightjs><code class="javascript">// epdUI 引入
+import Vue from 'vue';
+import epdUI from '@/components'
+Vue.use(epdUI)</code></pre>
+      </div>
+
+      <div class="secition">
+        <h3>按需引入</h3>
+        <pre v-highlightjs><code class="javascript">// epdUI 引入
+import Vue from 'vue';
+import { Button } from '@/components'
+Vue.use(Button)</code></pre>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <style lang="less" scope>
-.AppManage{}
-.titlePart {
-}
-.contPart{
-  margin-bottom: 10px;
-  .creatApp{
-    display: inline-block;
-    margin: 20px 0 5px;
-    color: #02A7F0;
-    font-size: 18px;
+.guide{
+  .secition{
+    padding: 0 10px;
+    margin-bottom: 50px;
   }
 }
 </style>
